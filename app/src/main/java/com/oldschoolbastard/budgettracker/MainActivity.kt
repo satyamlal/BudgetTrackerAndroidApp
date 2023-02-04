02cabc646c279b9241e0ed36beb01b805dec87bc
Satyam Lal <ssatyamlal@gmail.com>
2023-02-04 22:09:43 +0530

    initial commit

---
+++ app/src/main/java/com/oldschoolbastard/budgettracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oldschoolbastard.budgettracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

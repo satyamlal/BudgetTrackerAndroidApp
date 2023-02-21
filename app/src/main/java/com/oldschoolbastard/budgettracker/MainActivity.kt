ec45a15f3245f05cb5270b4762d08cb569df5817
Satyam Lal <ssatyamlal@gmail.com>
2023-02-21 17:22:51 +0530

    FAB added

--- app/src/main/java/com/oldschoolbastard/budgettracker/MainActivity.kt
+++ app/src/main/java/com/oldschoolbastard/budgettracker/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.oldschoolbastard.budgettracker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }

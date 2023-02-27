d14e5f698a588a4fa8c860bfcc7b0e3e9f3d4ab5
Satyam Lal <ssatyamlal@gmail.com>
2023-02-27 19:11:50 +0530

    working on resolved references for budget

--- app/src/main/java/com/oldschoolbastard/budgettracker/MainActivity.kt
+++ app/src/main/java/com/oldschoolbastard/budgettracker/MainActivity.kt
@@ -3,14 +3,17 @@ package com.oldschoolbastard.budgettracker
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
+import com.oldschoolbastard.budgettracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var transactions: ArrayList<Transaction>
     private lateinit var transactionAdapter: TransactionAdapter
     private lateinit var linearLayoutManager: LinearLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
 
@@ -25,9 +28,21 @@ class MainActivity : AppCompatActivity() {
         transactionAdapter = TransactionAdapter(transactions)
         linearLayoutManager = LinearLayoutManager(this)
 
-        recyclerview.apply {
+        binding.recyclerview.apply {
             adapter = transactionAdapter
             layoutManager = linearLayoutManager
         }
+        updateDashboard()
+    }
+
+    private fun updateDashboard() {
+
+        val totalAmount = transactions.map{ it.amount }.sum()
+        val budgetAmount = transactions.filter{ it.amount > 0 }.map{ it.amount }.sum()
+        val expenseAmount = totalAmount - budgetAmount
+
+        binding.total_balance!!.text = "₹%.2f".format(totalAmount)
+        binding.budget!!.text = "₹%.2f".format(budgetAmount)
+        binding.expense!!.text = "₹%.2f".format(expenseAmount)
     }
 }

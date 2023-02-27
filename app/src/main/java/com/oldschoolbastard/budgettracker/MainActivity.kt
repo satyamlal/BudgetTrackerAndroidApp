772fc31bb6c86c0f108dd7f67239fefb4a663eba
Satyam Lal <ssatyamlal@gmail.com>
2023-02-28 00:06:22 +0530

    binding is facing some issue

--- app/src/main/java/com/oldschoolbastard/budgettracker/MainActivity.kt
+++ app/src/main/java/com/oldschoolbastard/budgettracker/MainActivity.kt
@@ -14,8 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
-
+//        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         transactions = arrayListOf(
             Transaction("Weekend budget", 1100.00),
@@ -41,8 +41,8 @@ class MainActivity : AppCompatActivity() {
         val budgetAmount = transactions.filter{ it.amount > 0 }.map{ it.amount }.sum()
         val expenseAmount = totalAmount - budgetAmount
 
-        binding.total_balance!!.text = "₹%.2f".format(totalAmount)
-        binding.budget!!.text = "₹%.2f".format(budgetAmount)
-        binding.expense!!.text = "₹%.2f".format(expenseAmount)
+        binding.totalBalance.text = "₹%.2f".format(totalAmount)
+        binding.budgetLeft!!.text = "₹%.2f".format(budgetAmount)
+        binding.totalExpense!!.text = "₹%.2f".format(expenseAmount)
     }
 }

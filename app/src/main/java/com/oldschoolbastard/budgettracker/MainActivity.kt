7ae52986b23c7a71d96900c10ecb322e34c5ddd8
Satyam Lal <ssatyamlal@gmail.com>
2023-02-27 13:10:31 +0530

    synthetic binding added

--- app/src/main/java/com/oldschoolbastard/budgettracker/MainActivity.kt
+++ app/src/main/java/com/oldschoolbastard/budgettracker/MainActivity.kt
@@ -3,12 +3,27 @@ package com.oldschoolbastard.budgettracker
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity.main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var transactions : ArrayList<Transaction>
+    private lateinit var transactionAdapter: TransactionAdapter
+    private lateinit var layoutManager: LinearLayoutManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        transactions = arrayListOf(
+            Transaction("Weekend budget", 1100.00),
+            Transaction("Bananas", -30.00),
+            Transaction("Apples", -80.00),
+            Transaction("Breakfast", -150.00),
+            Transaction("Juice", -30.00),
+        )
+
+        transactionAdapter = TransactionAdapter(transactions)
+        layoutManager = LinearLayoutManager(this)
+
 
     }
 }
